1fdc4806259fe4bf2a58268b023a93768056ac1e
Mark <knezm1@mcmaster.ca>
2025-06-17 15:32:43 -0400

    Correct bit shift direction in matrix.c

--- matrix.c
+++ matrix.c
@@ -17,12 +17,12 @@ void circular_matrix_multiply (quasi_cyclic_matrix_t * H, vector_t x, vector_t b
         for (int i = 0; i < H->rowWeight[j]; i++) {
             
             if (H->lifting_size == 32) {
-                b.data32 [j] ^= (x.data32 [H->columnIndexMap[H->rowOffset[j]+i]] << H->base_graph[H->rowOffset[j]+i]%H->lifting_size) | 
-                (x.data32 [H->columnIndexMap[H->rowOffset[j]+i]] >> (H->lifting_size - H->base_graph[H->rowOffset[j]+i]%H->lifting_size));
+                b.data32 [j] ^= (x.data32 [H->columnIndexMap[H->rowOffset[j]+i]] >> H->base_graph[H->rowOffset[j]+i]%H->lifting_size) | 
+                (x.data32 [H->columnIndexMap[H->rowOffset[j]+i]] << (H->lifting_size - H->base_graph[H->rowOffset[j]+i]%H->lifting_size));
             }
             else if (H->lifting_size == 16) {
-                b.data16 [j] ^= (x.data16 [H->columnIndexMap[H->rowOffset[j]+i]] << H->base_graph[H->rowOffset[j]+i]%H->lifting_size) | 
-                (x.data16 [H->columnIndexMap[H->rowOffset[j]+i]] >> (H->lifting_size - H->base_graph[H->rowOffset[j]+i]%H->lifting_size));
+                b.data16 [j] ^= (x.data16 [H->columnIndexMap[H->rowOffset[j]+i]] >> H->base_graph[H->rowOffset[j]+i]%H->lifting_size) | 
+                (x.data16 [H->columnIndexMap[H->rowOffset[j]+i]] << (H->lifting_size - H->base_graph[H->rowOffset[j]+i]%H->lifting_size));
             }
             else {
                 b.data8 [j] ^= (x.data8 [H->columnIndexMap[H->rowOffset[j]+i]] >> H->base_graph[H->rowOffset[j]+i]%H->lifting_size) | 
