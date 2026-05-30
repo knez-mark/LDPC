5236e79573ea474d368a6e08fdcdc2ce06a157f2
Mark <knezm1@mcmaster.ca>
2026-05-29 23:57:00 -0400

    Remove old main file

--- main2.c
+++ main2.c
@@ -7,111 +7,11 @@
 #include "simulation.h"
 #include "LDPC.h"
 
-static void print_5bits(uint32_t value)
-{
-    for (int i = 4; i >= 0; i--) {
-        printf("%u", (value >> i) & 1);
-    }
-}
-
 int main() {
     
     float noise_stddev = 0.6f;
 
     srand((unsigned)time(NULL));
-
-    /*
-    //Static, zero-initialized struct
-    static message_t message = {0};
-
-    static encoded_t encoding = {0};
-    static encoded_t decoding = {0};
-
-    static uint8_t parity [272] = {0};
-
-    static float bpsk_symbols [32+128*8] = {0};
-    
-    static float ldpc_llr [68*32] = {0};
-
-    //Generate random message size: 0–87
-    message.msg_size = 21;//rand_u8_range(0, 87);
-
-    //Fill message payload
-    generate_random_bytes(message.msg, message.msg_size);
-
-    //Generate length field
-    encoding.length_field = generate_length_field(message.msg_size + 1);
-
-    LDPC_encode ((uint8_t *) &message, get_LDPC_len(encoding.length_field), parity);
-
-    format_encoded_data((uint8_t *) &message, parity, encoding.ldpc, encoding.length_field);
-
-    printf("msg_size      = %d\n", message.msg_size);
-    printf("length_field  = %x\n", encoding.length_field);
-    print_5bits(encoding.length_field);
-    printf("\n");
-
-    for (int i = 0; i < message.msg_size + 1; i++) {
-        if (i == 0) {
-            printf ("%x, ", message.msg_size);
-        }
-        else {
-            printf ("%x, ", message.msg [i-1]);
-        }
-        if ((i+1) % 8 == 0) {
-            printf ("\n");
-        }
-    }
-    
-
-    printf ("\n\n");
-
-    for (int i = 0; i < get_frame_len (encoding.length_field) - 4; i++) {
-        printf ("%x, ", encoding.ldpc [i]);
-        if ((i+1) % 8 == 0) {
-            printf ("\n");
-        }
-    }
-
-    uint16_t num_bytes = get_frame_len (encoding.length_field);
-    uint16_t num_bits = bytes_to_bpsk((uint8_t*) &encoding, num_bytes, bpsk_symbols);
-    add_awgn(bpsk_symbols, num_bits, noise_stddev);
-
-    for (int i = 32; i < num_bits; i++) {
-        printf ("%5.2f, ", bpsk_symbols [i]);
-        if ((i+1) % 8 == 0) {
-            printf ("\n");
-        }
-    }
-
-    decoding.length_field = len_field_decode_soft (bpsk_symbols);
-
-    printf("length_field  = %x\n", decoding.length_field);
-    printf ("\n");
-
-    format_decoded_data(bpsk_symbols + 32, ldpc_llr, decoding.length_field);
-
-    
-    for (int i = 0; i < get_LDPC_len(encoding.length_field); i++) {
-        printf ("%5.2f, ", ldpc_llr [i]);
-        if ((i+1) % 8 == 0) {
-            printf ("\n");
-        }
-    }
-
-    uint16_t num_iters;
-    printf ("parity_check_errors: %d\n", LDPC_decode (ldpc_llr, get_LDPC_len(decoding.length_field), decoding.ldpc, 50, &num_iters));
-    printf ("num_iters: %d\n", num_iters);
-
-    printf ("LDPC decoded: \n");
-
-    for (int i = 0; i < message.msg_size + 1; i++) {
-        printf ("%x, ", decoding.ldpc[i]);
-        if ((i+1) % 8 == 0) {
-            printf ("\n");
-        }
-    }
-    */
 
     struct timespec start, end;
     FILE *fp = fopen("sim_data.csv", "w"); 
